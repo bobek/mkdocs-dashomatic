148643ae8da5807c2e3594fbfa384c457d39761e
Antonin Kral <antonin.kral@ipfabric.io>
2022-06-29 21:18:15 +0200

    We require character in front of and after the --/--- to change them

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mkdocs-dashomatic',
-    version='0.1.1',
+    version='0.1.2',
     description='A MkDocs plugin to convert hyphens to dashes.',
     long_description=long_description,
     long_description_content_type="text/markdown",
